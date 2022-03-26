472b64168119d4261e24bc442bc5369b7c06e1a3
eyeofstorm <liuchengdan@outlook.jp>
2022-03-26 16:28:21 +0900

    npcbots sql files.

---
+++ sql/world/npc_text_npcbots.sql
@@ -0,0 +1,6 @@
+DELETE FROM `npc_text` WHERE `ID` IN (9000000, 9000001, 9000002);
+
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES
+(9000000, `There are always dudes ready to kill for money.`), 
+(9000001, `Mercenaries are always in demand. Here is what available right now.`),
+(9000002, `Mercenaries are always in demand. Here is what available right now.`);
